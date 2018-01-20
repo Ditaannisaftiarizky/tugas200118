f61d146159285fdb9c6fec14d7c13e6d48c4500c
Ditaannisaftiarizky <dita.annisft.r@gmail.com>
2018-01-20 15:58:52 -0800

    input class unggas

--- class_unggas.cpp
+++ class_unggas.cpp
@@ -2,6 +2,15 @@
 #include <string>
 using namespace std;
 
+class unggas {
+	public:
+		void suara(string bunyi);
+		int terbang(string sayap);
+		string renang;
+		void ukuranTelur(string ukuran);
+		void jumlahTelur(int jumlah);
+};
+
 int main (){
 	
 	
