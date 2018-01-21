43c46ab4f4280fa352d73c6dab12eb7228efdc40
Ditaannisaftiarizky <dita.annisft.r@gmail.com>
2018-01-20 19:09:40 -0800

    input class lipstik, void warna, void harga, int main

--- class_lipstik.cpp
+++ class_lipstik.cpp
@@ -2,6 +2,25 @@
 #include <string>
 using namespace std;
 
+class lipstik {
+	public:
+		string teksture;
+		void warna(string warna);
+		void harga(string harga);
+};
+
 int main (){
 	
-}
+	lipstik wardah,makeover,purbasari,nabi;
+	
+	wardah.teksture ="Matte";
+	makeover.teksture ="cair";
+	purbasari.teksture ="lembut";
+	nabi.teksture ="cair";
+	
+	cout << "Lipstik Wardah" ;
+	cout <<(wardah.teksture);
+	wardah.warna("Red-Dicted, Oh So Nude, Pink Me, See You Latte" );
+	wardah.harga("Rp. 62.000");
+	
+	}
