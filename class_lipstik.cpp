b59b59282a517a5667f9ff7251575518c583d135
Ditaannisaftiarizky <dita.annisft.r@gmail.com>
2018-01-20 19:26:52 -0800

    hasil akhir

--- class_lipstik.cpp
+++ class_lipstik.cpp
@@ -22,22 +22,31 @@ int main (){
 	cout <<(wardah.teksture);
 	wardah.warna("Red-Dicted, Oh So Nude, Pink Me, See You Latte" );
 	wardah.harga("Rp. 62.000");
+	cout <<"_______________________________\n\n";
 	
 	cout << "Lipstik Makeover" ;
-	cout <<(wardah.teksture);
+	cout <<(makeover.teksture);
 	makeover.warna("Retro Pink, Sassy Pink, Glorius Gold, Red Rhapsody" );
 	makeover.harga("Rp. 115.000");
+	cout <<"_______________________________\n\n";
 	
 	cout << "Lipstik Purbasari" ;
-	cout <<(wardah.teksture);
+	cout <<(purbasari.teksture);
 	purbasari.warna("Diamond, Mirah, Pinus, Jade");
 	purbasari.harga("Rp. 53.000");
+	cout <<"_______________________________\n\n";
 	
 	cout << "Lipstik Nabi" ;
-	cout <<(wardah.teksture);
+	cout <<(nabi.teksture);
 	nabi.warna("Honey, Natural, Mocha, Champagne" );
 	nabi.harga("Rp.35.000");
-	
-	
-	
+	cout <<"_______________________________\n\n";
 	}
+	
+void lipstik::warna(string warna){
+	cout <<"\nWarna yang Tersedia "<<warna<<endl;
+}
+void lipstik::harga(string harga){
+	cout <<"Harga: "<<harga<<endl;
+}
+
